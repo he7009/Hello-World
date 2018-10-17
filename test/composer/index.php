55cb26697dd7f8e66adfdbcd977c7273c9866f00
段育德 <dydphp@163.com>
2018-10-17 16:15:18 +0800

    增加忽略文件

---
+++ test/composer/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ¶ÎÓıµÂ
+ * Date: 2018/10/17
+ * Time: 12:23
+ */
+
+require_once './vendor/autoload.php';
+
+use Gregwar\Captcha\CaptchaBuilder;
+
+$builder = new CaptchaBuilder;
+$builder->build();
+//header('Content-type: image/jpeg');
+//$builder->save('out.jpg');
+$builder->output();
