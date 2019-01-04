d7b3b9abab09ef3643e6ae8720348c20045ca333
段育德 <dydphp@163.com>
2019-01-04 15:06:10 +0800

    进程调试

--- test/pcntl.php
+++ test/pcntl.php
@@ -9,7 +9,18 @@ class pcntl
 
     public function start()
     {
-        echo 11;die;
+        $ppid = posix_getpid();
+        $pid = pcntl_fork();
+        if ($pid == -1) {
+            throw new Exception('fork子进程失败!');
+        } elseif ($pid > 0) {
+            cli_set_process_title("我是父进程,我的进程id是{$ppid}.");
+            sleep(300); // 保持30秒，确保能被ps查到
+        } else {
+            $cpid = posix_getpid();
+            cli_set_process_title("我是{$ppid}的子进程,我的进程id是{$cpid}.");
+            sleep(300);
+        }
     }
 
 }
