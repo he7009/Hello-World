4c415c2c1d5b274e159c597384d44a20ba001764
段育德 <dydphp@163.com>
2019-01-04 15:42:25 +0800

    修改

--- test/pcntl.php
+++ test/pcntl.php
@@ -15,7 +15,7 @@ class pcntl
             throw new Exception('fork子进程失败!');
         } elseif ($pid > 0) {
             cli_set_process_title("我是父进程,我的进程id是{$ppid}.");
-            sleep(300); // 保持30秒，确保能被ps查到
+            sleep(3); // 保持30秒，确保能被ps查到
         } else {
             $cpid = posix_getpid();
             cli_set_process_title("我是{$ppid}的子进程,我的进程id是{$cpid}.");
