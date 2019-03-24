078f29634c8d9666134228f2d6a8950b244ae6ec
dyd <dydphp@163.com>
2019-03-25 07:33:10 +0800

    Yii2框架错误处理学习

---
+++ test/error/error.php
@@ -0,0 +1,67 @@
+<?php
+error_reporting(0);
+
+class erroraction
+{
+
+    public function run()
+    {
+        echo '222'.PHP_EOL;
+        var_dump(error_reporting());
+
+    }
+
+    public function index()
+    {
+
+        var_dump(error_reporting());
+        echo 'index_public'.PHP_EOL;
+        try{
+            Throw new Exception('我错了！哈哈哈哈哈');
+        }catch (Error $e){
+            echo 'catch';
+            echo $e->getCode().PHP_EOL;
+            echo $e->getMessage().PHP_EOL;
+            echo $e->getFile().PHP_EOL;
+        }
+        echo '异常之后执行代码';
+    }
+
+
+
+    /**
+     * 错误处理函数
+     */
+    public static function error_hanidle($errorno,$errorstr)
+    {
+        echo 'error_handle'.PHP_EOL;
+        echo 'errorno:'.$errorno.PHP_EOL;
+        echo 'errorstr:'.$errorstr.PHP_EOL;
+    }
+
+    /**
+     * 异常处理函数
+     */
+    public static function exception_handle(Throwable $e)
+    {
+        echo "exception_handle".PHP_EOL;
+        echo 'exception_msg:'.$e->getMessage().PHP_EOL;
+    }
+
+    /**
+     * 结束最后处理函数
+     */
+    public static function shutdown_handle()
+    {
+        echo 'shutdown_handle'.PHP_EOL;
+    }
+}
+
+
+set_error_handler(['erroraction','error_hanidle']);
+set_exception_handler(["erroraction",'exception_handle']);
+register_shutdown_function(['erroraction','shutdown_handle']);
+
+
+$erroraction = new erroraction();
+$erroraction->run();
