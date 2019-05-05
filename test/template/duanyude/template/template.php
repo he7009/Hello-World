99192d2338816a7ea4e0cd1de35924d49cde7e61
dyd <dydphp@163.com>
2019-05-05 22:45:11 +0800

    学习框架和模板引擎

---
+++ test/template/duanyude/template/template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP-PC
+ * Date: 2019/5/5
+ * Time: 22:35
+ */
+
+namespace duanyude\template;
+
+
+class template
+{
+    private $arrayConfig = [
+        'suffix' => '.m'  //设置模板文件后缀
+    ];
+
+}
---
+++ test/template/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP-PC
+ * Date: 2019/5/5
+ * Time: 22:39
+ */
+define("BASEPATH",__DIR__);
+define("application", BASEPATH . '/application');
