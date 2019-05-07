85dd24fe2fd15523dc07f132a085ee72a38f9c87
段育德 <dydphp@163.com>
2019-05-07 09:48:03 +0800

    继续学习模板引擎

--- test/template/duanyude/template/template.php
+++ test/template/duanyude/template/template.php
@@ -10,9 +10,24 @@ namespace duanyude\template;
 
 class template
 {
+    private $value = [];
     private $arrayConfig = [
         'suffix' => '.m',
-
     ];
+
+
+    public function assign($key,$val)
+    {
+        if(is_array($key)){
+            $this->value = array_merge($this->value,$key);
+        }else{
+            $this->value[$key] = $val;
+        }
+    }
+
+    public function show()
+    {
+
+    }
 
 }
