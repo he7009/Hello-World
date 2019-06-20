188f26469026f2df63378230cb78e89bbf8e3a20
dyd <dydphp@163.com>
2019-06-20 23:07:31 +0800

    首页初次提交

--- jk/index.php
+++ jk/index.php
@@ -1,17 +1,141 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: HP-PC
+ * Date: 2019/6/20
+ * Time: 21:59
+ */
 
-class jk{
+define("HOSTIMAGE",'//jk.helilan.cn/images/');
 
-    public function start(){
+class Jk{
 
+
+    public function index()
+    {
+        $banner = [
+            ['banner_url' => HOSTIMAGE . 'index/index-red.png'],
+            ['banner_url' => HOSTIMAGE . 'index/index-blue.png'],
+            ['banner_url' => HOSTIMAGE . 'index/index-white.png']
+        ];
+
+        //推荐
+        $recommendList = [
+            [
+                'name' => '啤酒',
+                'special_price' => '19.9',
+                'small_img' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'name' => '啤酒',
+                'special_price' => '19.9',
+                'small_img' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'name' => '啤酒',
+                'special_price' => '19.9',
+                'small_img' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'name' => '啤酒',
+                'special_price' => '19.9',
+                'small_img' => HOSTIMAGE . 'index/recom.png'
+            ],
+        ];
+
+        $adList = [
+            [
+                'banner_url' => HOSTIMAGE . 'index/adlist.png'
+            ],
+            [
+                'banner_url' => HOSTIMAGE . 'index/adlist.png'
+            ],
+            [
+                'banner_url' => HOSTIMAGE . 'index/adlist.png'
+            ],
+            [
+                'banner_url' => HOSTIMAGE . 'index/adlist.png'
+            ]
+        ];
+
+        $newList = [
+            [
+                'name' => '葡萄美酒',
+                'special_price' => '19.9',
+                'small_img' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'name' => '葡萄美酒',
+                'special_price' => '19.9',
+                'small_img' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'name' => '葡萄美酒',
+                'special_price' => '19.9',
+                'small_img' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'name' => '葡萄美酒',
+                'special_price' => '19.9',
+                'small_img' => HOSTIMAGE . 'index/recom.png'
+            ],
+        ];
+
+        $homeCategoryList = [
+            [
+                'cate_name' => '葡萄酒',
+                'special_price' => '19.9',
+                'icon_url' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'cate_name' => '葡萄酒',
+                'special_price' => '19.9',
+                'icon_url' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'cate_name' => '葡萄酒',
+                'special_price' => '19.9',
+                'icon_url' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'cate_name' => '葡萄酒',
+                'special_price' => '19.9',
+                'icon_url' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'cate_name' => '葡萄酒',
+                'special_price' => '19.9',
+                'icon_url' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'cate_name' => '葡萄酒',
+                'special_price' => '19.9',
+                'icon_url' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'cate_name' => '葡萄酒',
+                'special_price' => '19.9',
+                'icon_url' => HOSTIMAGE . 'index/recom.png'
+            ],
+            [
+                'cate_name' => '葡萄酒',
+                'special_price' => '19.9',
+                'icon_url' => HOSTIMAGE . 'index/recom.png'
+            ],
+        ];
+
+        $data['banner'] = $banner;
+        $data['recommendList'] = $recommendList;
+        $data['adList'] = $adList;
+        $data['newList'] = $newList;
+        $data['homeCategoryList'] = $homeCategoryList;
+
+
+        echo json_encode($data);
     }
 
 }
 
-$jk = new jk();
-
-$jk->start();
-
-
-
-
+$Jk = new Jk();
+$action = !empty($_GET['action']) ? $_GET['action'] : 'index';
+$Jk->$action();
