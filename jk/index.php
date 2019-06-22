a5609f732a3e671f15a92fb2355ac1acecbd246d
dyd <dydphp@163.com>
2019-06-22 08:33:37 +0800

    修改分类页返回结果

--- jk/index.php
+++ jk/index.php
@@ -134,8 +134,130 @@ class Jk{
         echo json_encode($data);
     }
 
+    public function cate()
+    {
+        $cateItems = [
+            [
+                'banner' => HOSTIMAGE . 'index/recom.png',
+                'cate_id' => 1,
+                'cate_name' => '红酒',
+                'ishaveChild' => true,
+                'children' => [
+                    [
+                        'child_id' => 1,
+                        'name' => '红酒-1'
+                    ],
+                    [
+                        'child_id' => 2,
+                        'name' => '红酒-2'
+                    ],
+                    [
+                        'child_id' => 3,
+                        'name' => '红酒-3'
+                    ],
+                ]
+            ],
+            [
+                'banner' => HOSTIMAGE . 'index/recom.png',
+                'cate_id' => 1,
+                'cate_name' => '白酒',
+                'ishaveChild' => true,
+                'children' => [
+                    [
+                        'child_id' => 1,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '白酒-1'
+                    ],
+                    [
+                        'child_id' => 2,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '白酒-2'
+                    ],
+                    [
+                        'child_id' => 3,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '白酒-3'
+                    ],
+                ]
+            ],
+            [
+                'banner' => HOSTIMAGE . 'index/recom.png',
+                'cate_id' => 1,
+                'cate_name' => '啤酒',
+                'ishaveChild' => true,
+                'children' => [
+                    [
+                        'child_id' => 1,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '啤酒-1'
+                    ],
+                    [
+                        'child_id' => 2,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '啤酒-2'
+                    ],
+                    [
+                        'child_id' => 3,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '啤酒-3'
+                    ],
+                ]
+            ],
+            [
+                'banner' => HOSTIMAGE . 'index/recom.png',
+                'cate_id' => 1,
+                'cate_name' => '饮料',
+                'ishaveChild' => true,
+                'children' => [
+                    [
+                        'child_id' => 1,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '饮料-1'
+                    ],
+                    [
+                        'child_id' => 2,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '饮料-2'
+                    ],
+                    [
+                        'child_id' => 3,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '饮料-3'
+                    ],
+                ]
+            ],
+            [
+                'banner' => HOSTIMAGE . 'index/recom.png',
+                'cate_id' => 1,
+                'cate_name' => '其他',
+                'ishaveChild' => true,
+                'children' => [
+                    [
+                        'child_id' => 1,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '其他-1'
+                    ],
+                    [
+                        'child_id' => 2,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '其他-2'
+                    ],
+                    [
+                        'child_id' => 3,
+                        'image' => HOSTIMAGE . 'index/recom.png',
+                        'name' => '其他-3'
+                    ],
+                ]
+            ],
+        ];
+
+//        $data['cateitems'] = $cateItems;
+        echo json_encode($cateItems);
+        exit;
+    }
+
 }
 
 $Jk = new Jk();
-$action = !empty($_GET['action']) ? $_GET['action'] : 'index';
+$action = !empty($_GET['action']) ? $_GET['action'] :( !empty($_POST['action']) ? $_POST['action'] : 'index');
 $Jk->$action();
