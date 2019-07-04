a7323249c1320e1f318138423fc832fc537174d2
dyd <dydphp@163.com>
2019-07-05 06:23:31 +0800

    java数据类型

---
+++ Java/src/First/First_first/FirstFirst.java
@@ -0,0 +1,7 @@
+package First.First_first;
+
+public class FirstFirst {
+    public static void main(String[] args) {
+        System.out.println("宝宝");
+    }
+}
---
+++ Java/src/Math.java
@@ -0,0 +1,7 @@
+public class Math {
+    public static void main(String[] args) {
+        long longVar = 88;
+        double sss = 33.0;
+        System.out.println(longVar/sss);
+    }
+}
