f6112e6e53ad255f59db61a659366289bb10ba3e
dyd <dydphp@163.com>
2019-08-20 23:17:08 +0800

    添加jk项目

---
+++ jk/controllers/QrcodeController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP-PC
+ * Date: 2019/8/17
+ * Time: 21:57
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class QrcodeController extends Controller
+{
+    //Éú³ÉQrCode
+    public function actionIndex()
+    {
+        return "QrCode";
+    }
+}
