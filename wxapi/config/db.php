2afa0a9b3c7e3801b21721c5b84af4f7983102ef
dyd <dydphp@163.com>
2019-10-13 21:52:00 +0800

    使用Gii生成activeRecord

--- wxapi/config/db.php
+++ wxapi/config/db.php
@@ -2,13 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=118.25.21.173;dbname=wxapp',
+    'dsn' => 'mysql:host=118.25.21.173;dbname=miniprogram',
     'username' => 'www',
     'password' => '123456',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'tablePrefix' => 'mini_',
 ];
---
+++ wxapi/models/IndexModel.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP-PC
+ * Date: 2019/10/13
+ * Time: 21:39
+ */
+
+namespace app\models;
+
+
+use yii\base\Model;
+use app\models\table;
+
+class IndexModel extends Model
+{
+    public function ad()
+    {
+        $tableAd = new table\Ad();
+        $data = $tableAd::find()->asArray()->all();
+        var_dump($data);
+        exit;
+    }
+}
