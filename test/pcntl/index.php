0279b3969b490b56224a8ee7615ccc4ae9b35e37
段育德 <dydphp@163.com>
2019-10-28 09:33:39 +0800

    php多进程扩展学习

--- test/pcntl/index.php
+++ test/pcntl/index.php
@@ -10,30 +10,18 @@ class index
 {
     public static function study()
     {
-        $array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,21,22,23,24];
-
-
-
-        $arr = array_chunk($array,4);
-
-        for ($i = 1;$i <= 10;$i++){
-            $pid = pcntl_fork();
-
-            if($pid == -1){
-                echo 'Create Fail';
-            }else if($pid == 0){
-                //子进程
-                echo "Child Process Success,Child Pid : " . posix_getpid() . ",Parent " . posix_getppid() . PHP_EOL;
-                sleep($i);
-                exit;
-            }else{
-
-
-            }
+        $pid = pcntl_fork();
+        if ($pid === 0) {
+            $myid = posix_getpid();
+            $parentId = posix_getppid();
+            fwrite(STDOUT, "my pid: $myid, parentId: $parentId\n");
+            sleep(5);
+            $myid = posix_getpid();
+            $parentId = posix_getppid();
+            fwrite(STDOUT, "my pid: $myid, parentId: $parentId\n");
+        } else {
+            fwrite(STDOUT, "parent exit\n");
         }
-
-        //父进程
-        pcntl_wait($status);
 
     }
 }
