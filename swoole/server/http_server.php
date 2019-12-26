6a8ce6a3da2248d639772880fc5e8d81dff1d9d8
dyd <dydphp@163.com>
2019-12-27 07:46:47 +0800

    swoole http static

--- swoole/server/http_server.php
+++ swoole/server/http_server.php
@@ -3,6 +3,8 @@
 
 $http = new swoole\http\server("0.0.0.0",8811);
 $http->set([
+    'enable_static_handler' => true,
+    'document_root' => '/vagrant/Hello-World/swoole/html',
     'daemonize' => 1
 ]);
 $http->on("request",function ($request,$response){
