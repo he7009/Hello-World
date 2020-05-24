10835a4a5aa06e223de91ff9f6a1a87dc6b07fcd
duanyude <dydphp@163.com>
2020-05-24 22:34:10 +0800

    删除多余文件

--- test/TL/project/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=47.103.137.236;dbname=jkpay',
-    'username' => 'JK-TL',
-    'password' => 'jk@JK-TL88',
-    'charset' => 'utf8',
-];
